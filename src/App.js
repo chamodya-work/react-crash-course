ea5845c2904735c5750c5e43b25be91574bbb16a
chamodya <chamodyarajapaksha1@gmail.com>
2025-04-26 14:36:40 +0530

    export product card components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      is it displaying?
+      <ProductCard />
+    </div>
+  );
+}
+function ProductCard() {
+  return (
+    <div className="product-card">
+      <img src="/images/iphone-5.png" alt="Product" className='product-image' />
+      <h2>Product Name</h2>
+      <p>Product Description</p>
+      <p>$19.99</p>
+      <button>Add to Cart</button>
     </div>
   );
 }
