e7acfb10b61d3bd446579dfb31eace6767746c81
chamodya <chamodyarajapaksha1@gmail.com>
2025-05-12 13:17:01 +0530

    add onChange and check target value in console log

--- src/components/ProductFilter.js
+++ src/components/ProductFilter.js
@@ -2,7 +2,9 @@ export function ProductFilter({ filter }) {
     return (
         <div>
             Price: $
-            <input defaultValue={filter.minPrice} type="number" min={0} max={100} /> -$
+            <input defaultValue={filter.minPrice} type="number" min={0} max={100}
+                onChange={(e) => console.log(e.target.value)}
+            /> -$
             <input defaultValue={filter.maxPrice} type="number" min={0} max={100} />
         </div>
     );
