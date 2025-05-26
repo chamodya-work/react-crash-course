c28da9171d80fab87b6516c2f3c3a1a19c824974
chamodya <chamodyarajapaksha1@gmail.com>
2025-05-26 16:05:11 +0530

    move product data to products.js

--- src/App.js
+++ src/App.js
@@ -2,45 +2,14 @@ import './App.css';
 import { ProductCard } from './components/ProductCard';
 import { ProductList } from './components/ProductList';
 import { ProductFilter } from './components/ProductFilter';
+import { products as productData } from './data/products';
 import { Fragment, useState } from 'react';
 
 
 function App() {
 
-  const products = [
-    {
-      id: 1,
-      imageSrc: "/images/iphone-5.png",
-      name: "iPhone 16 Pro Max",
-      specs: ["Long battery life", "Excellent camera", "A18 Pro chip"],
-      price: 1399,
-      stockCount: 5
-    },
-    {
-      id: 2,
-      imageSrc: "images/apple-tv.png",
-      name: "Samsung Galaxy S24 Ultra",
-      specs: ["200MP camera", "S Pen included", "Snapdragon 8 Gen 3"],
-      price: 1299,
-      stockCount: 3
-    },
-    {
-      id: 3,
-      imageSrc: "/images/apple-watch.png",
-      name: "Google Pixel 9 Pro",
-      specs: ["Pure Android experience", "Tensor G3 chip", "Outstanding photography"],
-      price: 1099,
-      stockCount: 0
-    },
-    {
-      id: 4,
-      imageSrc: "/images/macbook.png",
-      name: "Apple M06 laptop",
-      specs: ["Fast charging", "Smooth AMOLED display", "Snapdragon 8 Gen 3"],
-      price: 899,
-      stockCount: 1
-    }
-  ];
+  const [products, setProducts] = useState(productData);
+
   const [filters, setFilters] = useState({
     price: {
       min: 0,
---
+++ src/data/products.js
@@ -0,0 +1,34 @@
+export const products = [
+    {
+        id: 1,
+        imageSrc: "/images/iphone-5.png",
+        name: "iPhone 16 Pro Max",
+        specs: ["Long battery life", "Excellent camera", "A18 Pro chip"],
+        price: 1399,
+        stockCount: 5
+    },
+    {
+        id: 2,
+        imageSrc: "images/apple-tv.png",
+        name: "Samsung Galaxy S24 Ultra",
+        specs: ["200MP camera", "S Pen included", "Snapdragon 8 Gen 3"],
+        price: 1299,
+        stockCount: 3
+    },
+    {
+        id: 3,
+        imageSrc: "/images/apple-watch.png",
+        name: "Google Pixel 9 Pro",
+        specs: ["Pure Android experience", "Tensor G3 chip", "Outstanding photography"],
+        price: 1099,
+        stockCount: 0
+    },
+    {
+        id: 4,
+        imageSrc: "/images/macbook.png",
+        name: "Apple M06 laptop",
+        specs: ["Fast charging", "Smooth AMOLED display", "Snapdragon 8 Gen 3"],
+        price: 899,
+        stockCount: 1
+    }
+];
